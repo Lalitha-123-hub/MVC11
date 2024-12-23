2cb3c5a5029cabcbcda4b7b29a80241be74910da
dell <dell@DESKTOP-KB6FBG3>
2024-12-23 22:44:14 +0530

    Add project files.

---
+++ MVC11/Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC11.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            Employee e1 = new Employee()
+            {
+                Id = 9001,
+                EmployeeName = "John Miller",
+                Department = "Solutions",
+                Designation = "Developer",
+                Salary = 20000.00
+            };
+            return View(e1);
+        }
+        public ActionResult ShowEmployees()
+        {
+            List<Employee> emplist = new List<Employee>()
+        {
+            new Employee()
+            {
+            Id = 9001,
+            EmployeeName = "John Miller",
+            Department = "Solutions",
+            Designation = "Developer",
+            Salary = 20000.00
+            },
+            new Employee()
+            {
+            Id = 9002,
+            EmployeeName = "Sabresan",
+            Department = "HR",
+            Designation = "Manager",
+            Salary = 30000.00
+            },
+            new Employee()
+            {
+            Id = 9003,
+            EmployeeName = "Bala Subrmanyan",
+            Department = "Solutions",
+            Designation = "Lead",
+            Salary = 40000.00
+            }
+        };
+            return View(emplist);
+        }
+    }
+
+}
+
---
+++ MVC11/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC11
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+        public double Salary {get; set; }
+
+    }
+}
